fe810ce0f5891e0a9f9746d521f6dde409b7a7ba
PyroPoro <rickywen12345@gmail.com>
2024-01-13 14:21:35 -0500

    fixed score text

--- Incognito/Assets/Scripts/ScoreManager.cs
+++ Incognito/Assets/Scripts/ScoreManager.cs
@@ -11,7 +11,6 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TextMeshPro playerScoreObj = GetComponent<TMP_text>();
         UpdateScoreText(0);
     }
 
