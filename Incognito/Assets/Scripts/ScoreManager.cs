02ef491cb582ff1aa2e900dfd207353fbfb7899e
Anicca37 <sapphire.li2001@gmail.com>
2024-01-13 14:17:54 -0500

    added game score

--- Incognito/Assets/Scripts/ScoreManager.cs
+++ Incognito/Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score = 0;
+    public TMP_Text playerScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        TextMeshPro playerScoreObj = GetComponent<TMP_text>();
+        UpdateScoreText(0);
     }
 
     // Update is called once per frame
@@ -18,5 +22,13 @@ public class ScoreManager : MonoBehaviour
         {
             score += (int)Mathf.Round(Time.deltaTime);
         }
+        UpdateScoreText(score);
     }
+
+    private void UpdateScoreText(int score)
+    {
+        playerScoreText.text = "Score: " + score;
+    }
+
+
 }
