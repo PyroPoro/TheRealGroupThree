010e5cdd143c69ac5d5858b13eb209264acbf3ef
PyroPoro <rickywen12345@gmail.com>
2024-01-13 12:04:03 -0500

    camer can look and clamped

--- Incognito/Assets/Scripts/CameraController.cs
+++ Incognito/Assets/Scripts/CameraController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSensativity = 100f;
+    public Transform playerBody;
+    private float xRot = 0f;
+    private float yRot = 0f;
+    public float lookRange;
     void Start()
     {
-        
+        playerBody = GameObject.Find("Player").GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+        xRot -= mouseY;
+        yRot += mouseX;
+        xRot = Mathf.Clamp(xRot, -lookRange, lookRange);
+        yRot = Mathf.Clamp(yRot, -lookRange, lookRange);
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.transform.localRotation = Quaternion.Euler(0f, yRot, 0f);
     }
 }
